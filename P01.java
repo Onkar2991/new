145403f37993e8358674faa2464635ff447c3e77
Onkar <hariom.bijjamwar@gmail.com>
2023-07-05 12:20:14 +0530

    Added pattern prog.

---
+++ P01.java
@@ -0,0 +1,15 @@
+package Pattern;
+
+//package Pattern;
+
+public class P01 {
+    public static void main(String[] args) {
+        int a=5;
+        for(int i = 1; i <= a;i++) {
+            for(int j = i;j <= a;j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ P03.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class P03 {
+    public static void main(String[] args) {
+        int a=5;
+        for(int i = 1; i <= a;i++) {
+            for(int j = i;j <= a;j++) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
