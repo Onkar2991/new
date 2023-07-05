01013f3b7292eca7e0f0390a2c1b38c6bbb533c0
ShraddhaGhogare99 <136713757+ShraddhaGhogare99@users.noreply.github.com>
2023-07-05 12:39:02 +0530

    Update P01.java
    add print statement

--- P01.java
+++ P01.java
@@ -4,6 +4,7 @@ package Pattern;
 
 public class P01 {
     public static void main(String[] args) {
+        System.out.println("Pattern"):
         int a=5;
         for(int i = 1; i <= a;i++) {
             for(int j = i;j <= a;j++) {
